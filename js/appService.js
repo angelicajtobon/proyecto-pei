462171432c6d22f3e487aa8cd5a19542c4598e6c
angelica <angelicajtobon@gmail.com>
2020-01-18 14:07:34 -0500

    se modifica el appService

--- js/appService.js
+++ js/appService.js
@@ -1,23 +1,39 @@
-function servicio(url, metodo, parametros?){
+localStorage.contador=0;
+
+async function servicio(url, metodo, parametros?){
+
+	var data = "";
+
 	try{
 		
 		var token = localStorage.getItem('token');
-		var headers = new Headers();
-		
-		var data = fetch({			
-			headers: token
+		var headers = new Headers({
+			"token-auth" : token,
+			"username" : username
 		});
-		return data;
+		
+		var miInit = { 
+			   method: metodo,
+               headers: headers,
+               mode: 'cors',
+               cache: 'default' 
+           };
+
+
+		data = await fetch(url,miInit);
+		
 		
 	}catch (E){
-		if(E.status == 401){
-			for(var i=0; i=3;i++){
-				const token = servicio.call(login,metodo,parametros);
-				localStorage.setItem("token", token);
-			};
+		if(E.status == 401 && localStorage.contador <= 3){
+			const token = servicio.call(login,metodo,parametros);
+			localStorage.setItem("token", token);
+			localStorage.contador++;
+		
 		}else{
 			throw new Error(E);
 			window.location.href="./login";
 		}
 	}
+
+	return data;
 }
