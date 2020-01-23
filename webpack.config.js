1d6e2f0739069dec52fd51637c530cb0fb57b956
angelica <angelicajtobon@gmail.com>
2020-01-22 19:34:03 -0500

    actualizando config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require("path")
+const webpack = require('webpack'); 
+const path = require('path');
 
 
 const indextInput = './htlm/index.html';
@@ -7,9 +8,6 @@ const indexOutput = 'index.html';
 
 const identificacionInput = './htlm/identificacion.html';
 const identificacionOutput = 'identificacion.html'
-
-const identificacionModificarInput = './htlm/identificacionModificar.html';
-const identificacionModificarOutput = 'identificacionModificar.html'
 
 const identificacionModificarInput = './htlm/identificacionModificar.html';
 const identificacionModificarOutput = 'identificacionModificar.html'
@@ -55,166 +53,236 @@ const registroOutput = 'registro.html'
 
 
 
-const webpackInitConfig  = {
+module.exports = {
+	mode: "development",
+	entry: {
+		main: './js/datospei.js',
+		app: './js/app.js',
+		appService: './js/appService.js',   		
+		generar: './js/generar.js',
+		usuarios: './js/usuarios.js'
+	},
 	output: {
-			path: path.resolve(--dirname, "dist"),
-			filename: "bundle.js",
-			publicPath: "/dist",
-	}
+		path: path.resolve(__dirname, "dist")
+	},
+
+	plugins :[
+	new HtmlWebpackPlugin({  
+		filename: indexOutput, 
+		template: indextInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			nombre: 'Inicio',
+			titulo: 'Bienvenido a la Plataforma de Administración de Proyectos Educativos',
+			descripcion: 'Aquí podrá crear desde cero su P.E.I, o incorporar el documento ya creado de su Institución Educativa',
+			iniciarSesion: 'Iniciar Sesion',
+			email: 'Email',
+			contraseña: 'Contraseña',
+			olvidoUsuario: 'Olvide usuario y/o contraseña',
+			recuperar: 'Recuperar datos',
+			solicitarRegistro: 'Solicitar registro',
+			idead: 'Instituto de Educación a Distancia'					
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: identificacionOutput, 
+		template: identificacionInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			nombre: 'Identificacion',
+			titulo: 'Identificación',
+			principal: 'Principal',
+			identificacion: 'Identificación',
+			horizonteInst: 'Horizonte Institucional',
+			gestionDir: 'Gestión Directiva',
+			gestionPed: 'Gestión Pedagógica',
+			gestionATH: 'Gestión Administrativa y Talento Humano'
+			gestionCom: 'Gestión de la Comunidad',
+			cerrarCesion: 'Cerrar Cesion',
+			nombreInst: 'Nombre de la Institución Educativa',
+			codigoDane: 'Código DANE',
+			ayudaResena: ' Cómo hacer una reseña',
+			resenahist: 'Reseña Histórica',
+			ubicacionGeo: 'Ubicación Geográfica',
+			distintivo: 'Ingresar Escudo o Bandera',
+			buscar: 'Buscar',
+			himno: 'Himno',
+			liderazgo: 'Liderazgo',
+			guardar : 'Guardar',
+			continuar: 'Continuar',
+			idead: 'Instituto de Educación a Distancia',
+			ingresar : 'Ingresar',
+			registro: 'Registro'
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: identificacionModificarOutput, 
+		template: identificacionModificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			nombre: 'Modificar Identificacion',
+			consultar: 'Consultar Identificación',
+			titulo: 'Modificar Identificación',
+			nombreInst: 'Nombre de la Institución Educativa',
+			codigoDane: 'Código DANE',
+			resenahist: 'Reseña Histórica',
+			ubicacionGeo: 'Ubicación Geográfica',
+			distintivo: 'Ingresar Escudo o Bandera',
+			buscar: 'Buscar',
+			himno: 'Himno',
+			liderazgo: 'Liderazgo',
+			guardar : 'Guardar Modificaciones',
+			idead: 'Instituto de Educación a Distancia'
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: horizonteInstitucionalOutput, 
+		template: horizonteInstitucionalInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			nombre: 'Horizonte Institucional',
+			titulo: 'Horizonte Institucional',
+			principal: 'Principal',
+			identificacion: 'Identificación',
+			horizonteInst: 'Horizonte Institucional',
+			gestionDir: 'Gestión Directiva',
+			gestionPed: 'Gestión Pedagógica',
+			gestionATH: 'Gestión Administrativa y Talento Humano'
+			gestionCom: 'Gestión de la Comunidad',
+			cerrarCesion: 'Cerrar Cesion',
+			ayuda1: 'Preguntas basicas para hacer una misión',
+			mision: 'Misión',
+			ayuda2: 'Elementos de una Visión',
+			vision: 'Visión',
+			ayuda3: 'Ejemplos principios institucionales',
+			principios: 'Principios',
+			ayuda4: 'Que es?',
+			filosofia: 'Filosofía',
+			ayuda5: 'Concepto',
+			guardar : 'Guardar ',
+			culturaInstitucional: 'Cultura Institucional',
+			continuar: 'Continuar',
+			idead: 'Instituto de Educación a Distancia'
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: horizonteInstitucionalModificarOutput, 
+		template: horizonteInstitucionalModificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			nombre: 'Modificar Horizonte Institucional',
+			
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionPedagogicaOutput, 
+		template: gestionPedagogicaInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Gestión Pedagógica',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionPedagogicaModificarOutput, 
+		template: gestionComunidadModificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Modificar Gestión Pedagógica',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionDirectivaOutput, 
+		template: gestionDirectivaInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Gestión Directiva',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionDirectivaModificarOutput, 
+		template: gestionDirectivaModificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Gestión Directiva',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionComunidadOutput, 
+		template: gestionComunidadInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Gestión de la Comunidad',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionComunidadModificarOutput, 
+		template: gestionComunidadModificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Modificar Gestión de la Comunidad',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionAdministrativaTHOutput, 
+		template: gestionAdministrativaTHInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Gestión Administrativa',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: gestionAdministrativaTHModificarOutput, 
+		template: gestionAdministrativaTHModificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: 'Modificar Gestión Administrativa',
+		}
+	}),
+
+	new HtmlWebpackPlugin({  
+		filename: principalOutput, 
+		template: principalInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: ' ',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: modificarOutput, 
+		template: modificarInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: ' ',
+		}
+	}),
+	new HtmlWebpackPlugin({  
+		filename: registroOutput, 
+		template: registroInput,
+		inject: true,
+		hash: true,
+		templateParameters: {
+			titulo: ' ',
+		}
+	})
+
+	]	
 	
-	module ={
-		plugins :[
-			new HtmlWebpackPlugin({  
-				filename: indexOutput, 
-				template: indextInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Inicio',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: identificacionOutput, 
-				template: identificacionInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Identificación',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: identificacionModificarOutput, 
-				template: identificacionModificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Modificar Identificación',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: horizonteInstitucionalOutput, 
-				template: horizonteInstitucionalInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Horizonte Institucional',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: horizonteInstitucionalModificarOutput, 
-				template: horizonteInstitucionalModificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Modificar Horizonte Institucional',
-				}
-		  }),
-		   new HtmlWebpackPlugin({  
-				filename: gestionPedagogicaOutput, 
-				template: gestionPedagogicaInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Gestión Pedagógica',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionPedagogicaModificarOutput, 
-				template: gestionComunidadModificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Modificar Gestión Pedagógica',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionDirectivaOutput, 
-				template: gestionDirectivaInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Gestión Directiva',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionDirectivaModificarOutput, 
-				template: gestionDirectivaModificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Gestión Directiva',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionComunidadOutput, 
-				template: gestionComunidadInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Gestión de la Comunidad',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionComunidadModificarOutput, 
-				template: gestionComunidadModificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Modificar Gestión de la Comunidad',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionAdministrativaTHOutput, 
-				template: gestionAdministrativaTHInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Gestión Administrativa',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: gestionAdministrativaTHModificarOutput, 
-				template: gestionAdministrativaTHModificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: 'Modificar Gestión Administrativa',
-				}
-		  }),
-		  
-		  new HtmlWebpackPlugin({  
-				filename: principalOutput, 
-				template: principalInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: ' ',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: modificarOutput, 
-				template: modificarInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: ' ',
-				}
-		  }),
-		  new HtmlWebpackPlugin({  
-				filename: registroOutput, 
-				template: registroInput,
-				inject: true,
-				hash: true,
-				templateParameters: {
-				  titulo: ' ',
-				}
-		  })
-		  
-		]
-	
-	}
 };
 
 
 
-module.exports = webpackInitConfig;
